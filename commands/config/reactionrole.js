985f999278f203f794e90e8723d3280f4f27bdda
lxvongobsthndl <canis.ardet@web.de>
2021-01-05 03:42:50 +0100

    Implemented a basic language sys for reactionrole

--- commands/config/reactionrole.js
+++ commands/config/reactionrole.js
@@ -11,17 +11,28 @@ module.exports = {
     args: true,
     usage: '<role id> <reaction emoji>',
     aliases: ['rr'],
+    i18n: {
+        en: {
+            embedAuthor: "Request a role",
+            embedDescription: "To get the **%s** role react with %s"
+        },
+        de: {
+            embedAuthor: "Bekomme eine Rolle",
+            embedDescription: "Um die Rolle **%s** zu erhalten, reagiere auf diese Nachricht mit %s"
+        }
+    },
     execute(message, args, guildConfig) {
         console.log(message.author.username + ' called "config/reactionrole" command' + ((args.length > 0) ? ' with args: ' + args : '.'));
 
+        let LANG = this.determineLanguage(guildConfig.language);
         var role = message.guild.roles.cache.get(args[0]);
         var reaction = (args[1]) ? args[1] : emojiList[getRndInteger(0, emojiList.length)];
         if (role) {
             return message.channel.send(new DiscordMessageEmbed()
-                .setAuthor('Request a role')
+                .setAuthor(LANG.embedAuthor)
                 .setColor('#33cc33')
                 .setTitle(role.name)
-                .setDescription('To get the **' + role.name + '** role react with ' + reaction))
+                .setDescription(LANG.embedDescription)
                 .then(msg => {
                     if (reaction.startsWith('<')) {
                         reaction = reaction.split(':')[reaction.split(':').length - 1].substring(0, reaction.split(':')[reaction.split(':').length - 1].length - 1);
@@ -35,6 +46,14 @@ module.exports = {
                 });
         } else {
             return message.channel.send(new NoSuchRoleError(message.author, this.name, args, 'The role id provided could not be resolved to a role on this server.').getEmbed());
+        }
+    },
+    determineLanguage(configLanguage) {
+        if(this.i18n[configLanguage]) {
+            return this.i18n[configLanguage];
+        }
+        else {
+            return this.i18n.en;
         }
     }
 }
