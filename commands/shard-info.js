6082814e82e83bb0b2d8b90682983745b37c75c4
lxvongobsthndl <canis.ardet@web.de>
2021-02-23 21:55:16 +0100

    Added bot info command. Currently only option is guilds.

---
+++ commands/shard-info.js
@@ -0,0 +1,35 @@
+const ArgumentError = require("../errors/ArgumentError");
+const DiscordMessageEmbed = require('discord.js').MessageEmbed;
+
+/** Command: shard-info <option>
+ *  Shows information about the bot
+ */
+module.exports = {
+	name: 'shard-info',
+    description: 'Reloads a command',
+    args: true,
+    secret: true,
+    aliases: [],
+    explict: false,
+    usage: '<option>',
+    execute(message, args, guildConfig) {
+        if(message.author.id !== "313742410180198431") return;
+        const option = args[0].toLowerCase();
+        console.log(message.author.username + ' called "shard-info" command' + ((args.length > 0) ? ' with args: ' + args : '.'));
+        switch(option) {
+            case 'guilds':
+                var desc = 'This bot is currently on the following Servers: \n';
+                message.client.guilds.cache.each(guild => desc += guild.name + '\n');
+                return message.channel.send(new DiscordMessageEmbed()
+                    .setColor('#0099ff')
+                    .setAuthor(message.client.user.tag, message.client.user.displayAvatarURL())
+                    .setDescription(desc)
+                    .setTimestamp());
+            default:
+                return message.channel.send(new ArgumentError(message.author, this.name, args, 'The argument provided does not match any known option.').getEmbed());
+        }
+
+        
+
+    }
+};
