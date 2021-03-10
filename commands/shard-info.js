7e65eaa7d7851b1c02518e908f9a6a1922cb96cc
Alexander Schreiner <lxvongobsthndlE@users.noreply.github.com>
2021-03-10 16:03:06 +0100

    Added basic version command to shard-info

--- commands/shard-info.js
+++ commands/shard-info.js
@@ -1,12 +1,14 @@
 const ArgumentError = require("../errors/ArgumentError");
 const DiscordMessageEmbed = require('discord.js').MessageEmbed;
+const fetch = require('node-fetch');
+const version = require('../configuration/bot-version.json');
 
 /** Command: shard-info <option>
  *  Shows information about the bot
  */
 module.exports = {
 	name: 'shard-info',
-    description: 'Reloads a command',
+    description: 'Shows information about the bot',
     args: true,
     secret: true,
     aliases: [],
@@ -18,7 +20,7 @@ module.exports = {
         console.log(message.author.username + ' called "shard-info" command' + ((args.length > 0) ? ' with args: ' + args : '.'));
         switch(option) {
             case 'guilds':
-                var desc = new DiscordMessageEmbed()
+                let desc = new DiscordMessageEmbed()
                     .setColor('#0099ff')
                     .setAuthor(message.client.user.tag, message.client.user.displayAvatarURL())
                     .setDescription('This bot is currently on the following Servers:')
@@ -26,9 +28,16 @@ module.exports = {
                 message.client.guilds.cache.each(guild => {
                     if(guild.available) 
                         desc.addField(guild.name,
-                            'Members: ' + guild.memberCount + 
-                            '\nCreated: ' + guild.createdAt);
+                            'Members: ' + guild.memberCount);
                 });
+                return message.channel.send(desc);
+            case "version":
+                let desc = new DiscordMessageEmbed()
+                    .setColor('#0099ff')
+                    .setAuthor(message.client.user.tag, message.client.user.displayAvatarURL())
+                    .setTimestamp();
+                //read version file from github to compare running version with most recent version
+                desc.setDescription('The bot is currently running version ' + version.version + '\nThe most recent version available is: {gitVersion}');
                 return message.channel.send(desc);
             default:
                 return message.channel.send(new ArgumentError(message.author, this.name, args, 'The argument provided does not match any known option.').getEmbed());
