ab67c5dd16ef4171dc2b14339081d73097420adc
play.js <git@gituser.com>
2021-03-30 09:57:51 +0200

    progress in event loader

--- classes/util/ClientEventLoader.js
+++ classes/util/ClientEventLoader.js
@@ -1,8 +1,6 @@
 const requireEvent = (event) => require('../events/' + event);
 module.exports = client => {
     client.on('ready', () => requireEvent('ready')(client));
-    client.on('reconnecting', () => requireEvent('reconnecting')(client));
-    client.on('shardDisconnect', () => requireEvent('disconnect')(client));
-    client.on('message', requireEvent('message'));
+    //client.on('message', requireEvent('message'));
     client.on('guildCreate', requireEvent('guildCreate'))
 };
