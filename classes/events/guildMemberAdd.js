4b5f62840e6f55194f9ce9699188233bd7041d82
lxvongobsthndl <canis.ardet@web.de>
2021-05-24 01:38:55 +0200

    Added join role feature Testing in production lets go

--- classes/events/guildMemberAdd.js
+++ classes/events/guildMemberAdd.js
@@ -5,14 +5,18 @@ module.exports = async (member, discordClient) => {
     const guildConfig = discordClient.guildManager.getGuildConfigById(member.guild.id);
     const welcomeChannel = member.guild.channels.cache.get(guildConfig.welcomeChannelId);
     const memberLog = member.guild.channels.cache.get(guildConfig.memberLogChannelId);
+    const joinRole = member.guild.roles.cache.get(guildConfig.joinRole);
     guildConfig.welcomeMessage = fill_welcome_msg(member, new DiscordMessageEmbed(guildConfig.welcomeMessage));
 
     // Send the message to a designated channel on a server:
     console.log(member.displayName + ' joined ' + member.guild.name + '.');
-    if(welcomeChannel) {
+    if (welcomeChannel) {
         welcomeChannel.send(guildConfig.welcomeMessage);
     }
-    if(memberLog) {
+    if (memberLog) {
         memberLog.send(member_joined(member, discordClient));
+    }
+    if (joinRole) {
+        member.roles.add(joinRole, "Join Role");
     }
 }
---
+++ commands/config/joinrole.js
@@ -0,0 +1,26 @@
+const ArgumentError = require('../../errors/ArgumentError');
+const DiscordMessageEmbed = require('discord.js').MessageEmbed;
+
+/** Command: joinrole
+ *  Give a role automatically on join.
+ */
+module.exports = {
+    name: 'joinrole',
+    description: 'Give a role automatically on join.',
+    args: true,
+    usage: '<role id>',
+    aliases: [],
+    execute(message, args, guildConfig) {
+        console.log(message.author.username + ' called "config/joinrole" command' + ((args.length > 0) ? ' with args: ' + args : '.'));
+        if (!message.guild.roles.cache.has(args[0])) {
+            return message.channel.send(new ArgumentError(message.author, this.name, args, 'The provided role id does not match a role on this server!'))
+        }
+        message.client.guildManager.updateGuildConfigById(guildConfig.guildId, 'joinRole', args[0]);
+        message.channel.send(new DiscordMessageEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL())
+            .setColor('#33cc33')
+            .setTimestamp()
+            .setTitle('Updated join role')
+            .setDescription('The new join role for this server is: #' + message.guild.roles.cache.get(args[0]).name));
+    }
+}
