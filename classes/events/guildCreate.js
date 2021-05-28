b0ec86ff258618fa362d98ba0dfe89e96d0557a9
lxvongobsthndlE <canis.ardet@web.de>
2021-05-28 23:30:18 +0200

    Automatically add bot admin role to guild owner on bot join

--- classes/events/guildCreate.js
+++ classes/events/guildCreate.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+
 module.exports = async guild => {
   let client = guild.client
   let aTextChannel = guild.channels.cache.find(ch => ch.type === 'text');
@@ -7,7 +8,7 @@ module.exports = async guild => {
 
   if (owner !== client.config.ownerId) {
     let channel = client.channels.cache.get(guild.systemChannelID || aTextChannel.id);
-    channel.send('Thanks for inviting me into this server!\nPlease use `' + client.config.prefix + 'info` and `' + client.config.prefix + 'help` for the information you need in order to set up and use the bot.\n\nHave any suggestions or noticed any bugs? Use `' + client.config.prefix + 'suggest` or `' + client.config.prefix + 'bug` to report them to my developer. \nThank you and have fun using the bot!');
+    channel.send('Thanks for inviting me into this server!\nPlease use `' + client.config.prefix + 'help` and `' + client.config.prefix + 'config help` for the information you need in order to set up and use the bot.\n\nHave any suggestions or noticed any bugs? Use `' + client.config.prefix + 'suggest` or `' + client.config.prefix + 'bug` to report them to my developer. \nThank you and have fun using the bot!');
 
     let blacklist = JSON.parse(fs.readFileSync('./botData/blacklist.json', 'utf8'));
     if (!blacklist[guild.ownerID]) return;
@@ -16,5 +17,9 @@ module.exports = async guild => {
       channel.send('But UNFORTUNATELY, the owner of this server has been blacklisted by my developer, so I\'m going to have to leave! Bye!');
       guild.leave();
     }
+
+    //void call to create guildconfig.
+    client.guildManager.getGuildConfigById(guild.id);
+    client.guildManager.addGuildConfigListElementById(guild.id, 'ADMIN_IDS', guild.ownerID);
   }
 }
