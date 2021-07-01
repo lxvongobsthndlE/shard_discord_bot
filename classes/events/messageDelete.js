52de11c4d47be40ed7218703c4b37508c111636c
lxvongobsthndlE <canis.ardet@web.de>
2021-07-01 18:14:20 +0200

    Added deleted messages logging

--- classes/events/messageDelete.js
+++ classes/events/messageDelete.js
@@ -1,3 +1,12 @@
 module.exports = async (message) => {
-    console.log('A message from ' + message.author.username + ' was deleted.');
+    console.log(`[MSG-DEL] ${message.author.username}: "${message.content}"`);
+    /*
+    let guildConfig = message.client.guildManager.getGuildConfigById(message.channel.guild.id);
+    if (guildConfig.modLogChannelId !== "" && guildConfig.modLogSendMessageDelete) {
+        let channel = message.guild.channels.resolve(guildConfig.modLogChannelId);
+        if (channel) {
+            console.log('A message from ' + message.author.username + ' was deleted.');
+        }
+    }
+    */
 }
